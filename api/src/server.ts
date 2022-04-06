aeb3cc84f5deeea177506718877f540cc2067667
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2022-04-06 15:50:29 -0300

    Fully working version

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,19 +2,20 @@ import express from 'express';
 const app = express();
 
 /* Note: Kafka dependencies */
-import { Kafka } from 'kafkajs'
+import { CompressionTypes, Kafka, logLevel } from 'kafkajs'
 
 const kafka = new Kafka({
     clientId: 'api',
     brokers: ['localhost:9092'],
     retry: {
-        initialRetryTime: 3000,
+        initialRetryTime: 300,
         retries: 10
-    }
+    },
+    // logLevel: logLevel.NOTHING
 });
 
 const producer = kafka.producer();
-
+const consumer = kafka.consumer({ groupId: 'certificate-group-receiver' });
 
 /* Note: Middleware of producer to all routes */
 app.use((req, res, next) => {
@@ -34,6 +35,7 @@ app.post('/generate-certificate', async (req, res) => {
     /* TODO: Call microservice */
     await req['producer'].send({
         topic: 'issue-certificate',
+        compression: CompressionTypes.GZIP,
         messages: [{ value: JSON.stringify(message) }]
     });
 
@@ -43,6 +45,14 @@ app.post('/generate-certificate', async (req, res) => {
 const run = async () => {
 
     await producer.connect();
+    await consumer.connect();
+    await consumer.subscribe({ topic: 'certification-response' });
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log('Message', message);
+        }
+    });
 
     app.listen(3000, () => console.log('Running on port 3000'));
 }
--- certification/src/main.ts
+++ certification/src/main.ts
@@ -6,15 +6,26 @@ const kafka = new Kafka({
 });
 
 const topic = 'issue-certificate';
+const producer = kafka.producer();
 const consumer = kafka.consumer({ groupId: 'certificate-group' });
 
 const run = async () => {
+    await producer.connect();
     await consumer.connect();
     await consumer.subscribe({ topic });
     await consumer.run({
         eachMessage: async ({ topic, partition, message }) => {
-            const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`;
+            const prefix = `${topic}[${partition}|${message.offset}] / ${message.timestamp}`;
             console.log(`- ${prefix} ${message.key}#${message.value}`);
+
+            const payload = JSON.parse(message.value.toString());
+
+            setTimeout(async () => {
+                await producer.send({
+                    topic: 'certification-response',
+                    messages: [{ value: `Certificate of user ${payload.user.name}` }]
+                });
+            }, 3000);
         }
     });
 }
